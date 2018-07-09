fba623103b462e690e0da7cc07cc1842602519ab
bobangol <gboban@gmail.com>
2018-07-09 18:51:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androiddevelopment/slobodangolocorbin/FirstActivity.java
@@ -0,0 +1,9 @@
+package com.example.androiddevelopment.slobodangolocorbin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+
+}
