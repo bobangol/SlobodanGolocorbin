9b4362b5784c951b588607b4ba73527d7f3434c6
bobangol <gboban@gmail.com>
2018-07-09 19:42:48 +0200

    Prva aktivnost, dialog, baza

---
+++ app/src/main/java/com/example/androiddevelopment/slobodangolocorbin/SecondActivity.java
@@ -0,0 +1,4 @@
+package com.example.androiddevelopment.slobodangolocorbin;
+
+public class SecondActivity {
+}
--- app/src/main/java/com/example/androiddevelopment/slobodangolocorbin/dialog/AboutDialog.java
+++ app/src/main/java/com/example/androiddevelopment/slobodangolocorbin/dialog/AboutDialog.java
@@ -12,7 +12,7 @@ public class AboutDialog extends AlertDialog.Builder{
         super(context);
 
         setTitle(R.string.dialog_about_title);
-        setMessage("Milos Simic");
+        setMessage("Slobodan Golocorbin");
         setCancelable(false);
 
         setPositiveButton(R.string.dialog_about_yes, new DialogInterface.OnClickListener() {
